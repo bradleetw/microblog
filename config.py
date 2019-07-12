d258f22a6768ffe99b817047891af18b35fd769f
brad lee <brad.lee.tw@outlook.com>
2019-07-12 11:03:25 +0800

    create database and meta-database

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     '''
     Flask app config class.
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'devfortesting'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     PP_KEY_TESTING = 'Just for testing.'
